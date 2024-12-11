39e2b734e4e5a9a801bb07592a9eee071b5f0aa2
glyeerr <enzoc.bronzon@gmail.com>
2024-12-10 21:31:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+urlsite = "https:/ozenvitta.onrender.com"
+
 
 @app.route('/')
 def homepage():
@@ -42,7 +44,7 @@ def enviar_pag_checkout4():
 ######
 def create_preference():
 
-    sdk = mercadopago.SDK("TEST-1661045138271582-120207-b060fd8b1801c0f2b79791af0340c35b-1833487732")
+    sdk = mercadopago.SDK("APP_USR-5715772148343132-112520-1462b5edd9366b7fe84b39beb42f0257-1833487732")
 
     payment_data = {
         "items": [
@@ -50,13 +52,13 @@ def create_preference():
                 "id": "1",
                 "title": "Produto de Teste",  # Nome do produto
                 "quantity": 1,                # Quantidade
-                "unit_price": 147.00,          # Preço unitário
+                "unit_price": 1.00,          # Preço unitário
             }
         ],
         "back_urls": {
-            "success": "https://localhost:3000/success",  # URL de sucesso
-            "failure": "https://www.seusite.com/failure",  # URL de falha
-            "pending": "https://www.seusite.com/pending",  # URL de pendente
+            "success": urlsite+"/success",  # URL de sucesso
+            "failure": urlsite+"/failure",  # URL de falha
+            "pending": urlsite+"/pending",  # URL de pendente
         },
         "auto_return": "approved",  # Retorno automático quando o pagamento for aprovado
     }
@@ -80,16 +82,16 @@ def create_preference2():
     payment_data = {
         "items": [
             {
-                "id": "1",
+                "id": "2",
                 "title": "Produto de Teste",  # Nome do produto
                 "quantity": 1,                # Quantidade
-                "unit_price": 197.00,          # Preço unitário
+                "unit_price": 1.00,          # Preço unitário
             }
         ],
         "back_urls": {
-            "success": "https://localhost:3000/success",  # URL de sucesso
-            "failure": "https://www.seusite.com/failure",  # URL de falha
-            "pending": "https://www.seusite.com/pending",  # URL de pendente
+            "success": urlsite+"/success",  # URL de sucesso
+            "failure": urlsite+"/failure",  # URL de falha
+            "pending": urlsite+"/pending",  # URL de pendente
         },
         "auto_return": "approved",  # Retorno automático quando o pagamento for aprovado
     }
@@ -108,21 +110,21 @@ def create_preference2():
 ###########
 def create_preference3():
 
-    sdk = mercadopago.SDK("TEST-1661045138271582-120207-b060fd8b1801c0f2b79791af0340c35b-1833487732")
+    sdk = mercadopago.SDK("APP_USR-5715772148343132-112520-1462b5edd9366b7fe84b39beb42f0257-1833487732")
 
     payment_data = {
         "items": [
             {
-                "id": "1",
+                "id": "3",
                 "title": "Produto de Teste",  # Nome do produto
                 "quantity": 1,                # Quantidade
-                "unit_price": 247.00,          # Preço unitário
+                "unit_price": 1.00,          # Preço unitário
             }
         ],
         "back_urls": {
-            "success": "https://localhost:3000/success",  # URL de sucesso
-            "failure": "https://www.seusite.com/failure",  # URL de falha
-            "pending": "https://www.seusite.com/pending",  # URL de pendente
+            "success": urlsite+"/success",  # URL de sucesso
+            "failure": urlsite+"/failure",  # URL de falha
+            "pending": urlsite+"/pending",  # URL de pendente
         },
         "auto_return": "approved",  # Retorno automático quando o pagamento for aprovado
     }
@@ -141,7 +143,7 @@ def create_preference3():
 ########
 def create_preference4():
 
-    sdk = mercadopago.SDK("TEST-1661045138271582-120207-b060fd8b1801c0f2b79791af0340c35b-1833487732")
+    sdk = mercadopago.SDK("APP_USR-5715772148343132-112520-1462b5edd9366b7fe84b39beb42f0257-1833487732")
 
     payment_data = {
         "items": [
@@ -149,13 +151,13 @@ def create_preference4():
                 "id": "1",
                 "title": "Produto de Teste",  # Nome do produto
                 "quantity": 1,                # Quantidade
-                "unit_price": 377.00,          # Preço unitário
+                "unit_price": 1.00,          # Preço unitário
             }
         ],
         "back_urls": {
-            "success": "https://localhost:3000/success",  # URL de sucesso
-            "failure": "https://www.seusite.com/failure",  # URL de falha
-            "pending": "https://www.seusite.com/pending",  # URL de pendente
+            "success": urlsite+"/success",  # URL de sucesso
+            "failure": urlsite+"/failure",  # URL de falha
+            "pending": urlsite+"/pending",  # URL de pendente
         },
         "auto_return": "approved",  # Retorno automático quando o pagamento for aprovado
     }
@@ -174,4 +176,4 @@ def create_preference4():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
